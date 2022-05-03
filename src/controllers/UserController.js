046c24d32827a6bf26daf7e6816ba107d923fa1e
Jeef-js <jeffersonthiago.ga@gmail.com>
2022-05-03 18:50:56 -0300

    adding login method to the UserController.js and fixing the duplicate email entry error.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     async store(req, res) { // Create a new user
@@ -48,23 +49,30 @@ module.exports = {
     },
 
     async login(req, res) { // Login a specific user
-        const { email, password_hash } = req.body;
-        if (!email || !password_hash) {
+        const { email, password } = req.body;
+         
+        if (!email || !password) {
             return res.json({ message: 'Email and password are required' });
         }
 
         const user = await User.findOne({
             where: {
                 email,
-                password_hash,
             },
         });
 
         if (!user) {
-            return res.json({ message: 'User not found' });
+            return res.status(400).json({ message: 'User not found' });
         }
 
-        return res.json(user);
+        if(!await bcrypt.compare(password, user.password_hash)) {
+            return res.status(400).json({ message: 'Invalid password' });
+        }
+
+        return res.status(200).json({ 
+            message: "Done!",
+            user
+        });
     },
 
     async verifyEmail(req, res) { // Verify a specific user email
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,11 @@ const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
-routes.post('/register', UserController.store);
+routes.post('/users/register', UserController.store);
+routes.post('/users/login', UserController.login);
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.delete('/users/:id', UserController.destroy);
 routes.patch('/users/:id', UserController.update);
-routes.post('/login', UserController.login);
 
 module.exports = routes;
